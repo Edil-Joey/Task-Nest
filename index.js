b9e437c759ae0cce241259c5c5bb6e40c251ecbb
Edil-Joey <ediljoey2002@gmail.com>
2026-07-20 23:34:41 +0530

    Updated the dashboard tab using html and css. Then updating current date, day using js and adding classname dash to cut the text of checkbox in Recent task.

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-let date=new Date;
-let day=date.getDay;
-let currentdate=date.getDate;
-let year=date.getFullYear
-console.log(day, currentdate,year)
+const day_name=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months_name=["January","February","March","April","May","June","July","August","September","October","November","December"];
+let date=new Date();
+let day=day_name[date.getDay()];
+let currentdate=date.getDate();
+let year=date.getFullYear();
+let month=months_name[date.getMonth()];
+
+document.getElementById("date").innerHTML=(day +", "+ currentdate +" "+month+" "+year );
+//Saturday, 11 July 2026
+
+
+function recent_task_checked(val){
+    let recent_task_checkbox=document.querySelector(".recent_task_name");
+    if(val.checked){
+        recent_task_checkbox.classList.add("dash");
+        console.log("dashed")
+    }
+    else{
+           recent_task_checkbox.classList.remove('dash');
+console.log("undash");
+
+    }
+    
+}
