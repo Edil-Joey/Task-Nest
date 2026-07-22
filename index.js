121db40c60429a6ad4a24b406142fbfcaddc38ac
Edil-Joey <ediljoey2002@gmail.com>
2026-07-22 22:55:20 +0530

    Slight work done in navigation bar, while clicking will display none to all page and set the selected page as display block

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ let month=months_name[date.getMonth()];
 document.getElementById("date").innerHTML=(day +", "+ currentdate +" "+month+" "+year );
 //Saturday, 11 July 2026
 
+const pages=['dashboard','tasks','tracker','progress']
+()=>{
+
+}
 
 function recent_task_checked(val){
     let recent_task_checkbox=document.querySelector(".recent_task_name");
@@ -17,9 +21,22 @@ function recent_task_checked(val){
         console.log("dashed")
     }
     else{
-           recent_task_checkbox.classList.remove('dash');
-console.log("undash");
+        recent_task_checkbox.classList.remove('dash');
+        console.log("undash");
 
     }
     
 }
+
+// page display will remove the select class(in navigation bar to show as selected).
+//make the selected page to display and other page to display none.
+function page_display(page,el){
+
+    document.querySelectorAll(".nav").forEach(nav=>nav.classList.remove("select"));
+    el.classList.add("select");
+   
+    document.querySelectorAll(".page").forEach(page=>page.style.display='none');
+    document.getElementsByClassName(page)[0].style.display='block';
+   
+}
+
